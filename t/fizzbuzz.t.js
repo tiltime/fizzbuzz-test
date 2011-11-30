913b8d97eece953981106a7a930f239fe2c9aa3b
Tiltime <ruangyotth@gmail.com>
2011-11-30 11:15:56 +0700

    Added return integer test case

--- t/fizzbuzz.t.js
+++ t/fizzbuzz.t.js
@@ -4,4 +4,5 @@ load('lib/fizzbuzz.js');
         ok( fizzbuzz(3) == "fizz", " 3 divide by 3 and return fizz" );
         ok( fizzbuzz(5) == "buzz", " 5 divide by 5 and return fizz" );
         ok( fizzbuzz(15) == "fizz buzz", " 15 can divie by 3 and 5 then return fizz buzz");
+        ok( fizzbuzz(1) == 1, "return integer if doesn't match with any case");
     });
