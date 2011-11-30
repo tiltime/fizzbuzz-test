15a1d80cae5cc02c2e5f4ff230beac852e7c1555
Tiltime <ruangyotth@gmail.com>
2011-11-30 11:02:01 +0700

    Added code for buzzz case

--- t/fizzbuzz.t.js
+++ t/fizzbuzz.t.js
@@ -2,5 +2,5 @@ plan(2);
 load('lib/fizzbuzz.js');
     test('fizzbuzz()', function() {
         ok( fizzbuzz(3) == "fizz", " 3 divide by 3 and return fizz" );
-        ok( fizzbuzz(5) == "buzza", " 5 divide by 5 and return fizz" );
+        ok( fizzbuzz(5) == "buzz", " 5 divide by 5 and return fizz" );
     });
