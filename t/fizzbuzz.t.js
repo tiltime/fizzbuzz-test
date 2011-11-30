f121d9a56d1de4f1acdc5586f58640436e8b2b42
Tiltime <ruangyotth@gmail.com>
2011-11-30 11:07:29 +0700

    Added fizzbuzz test case

--- t/fizzbuzz.t.js
+++ t/fizzbuzz.t.js
@@ -1,6 +1,7 @@
-plan(2);
+plan(3);
 load('lib/fizzbuzz.js');
     test('fizzbuzz()', function() {
         ok( fizzbuzz(3) == "fizz", " 3 divide by 3 and return fizz" );
         ok( fizzbuzz(5) == "buzz", " 5 divide by 5 and return fizz" );
+        ok( fizzbuzz(15) == "fizz buzz", " 15 can divie by 3 and 5 then return fizz buzz");
     });
